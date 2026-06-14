e436347726e3a802ebf2cc96b815ec404dd31ed8
makimakiver <yutaka3209@gmail.com>
2026-06-14 22:48:39 +0900

    test: drop unused numpy import in dispatch test

--- tests/test_env_dispatch.py
+++ tests/test_env_dispatch.py
@@ -5,8 +5,6 @@ from __future__ import annotations
 import os
 import sys
 
-import numpy as np
-
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from pollius.config import PolliusConfig
